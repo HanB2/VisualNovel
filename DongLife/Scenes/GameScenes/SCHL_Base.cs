19b57ae8a9f0312381e0333dd7bc29f7367df543
Trevor Fisher <minalear@gmail.com>
2016-03-30 13:26:43 -0500

    Cleanup and began working on Janitor/Principal branch

--- DongLife/Scenes/GameScenes/SCHL_Base.cs
+++ DongLife/Scenes/GameScenes/SCHL_Base.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenTK;
+using DongLife.Code;
+using DongLife.Controls;
 
 namespace DongLife.Scenes.GameScenes
 {
     public class SCHL_Base : VNScene
     {
-        public SCHL_Base() : base("SCHL_Base") { }
+        private Actor player, janitor, principal;
+
+        public SCHL_Base() : base("SCHL_Base")
+        {
+            background = new Background(@"Textures/Backgrounds/school_hall.png");
+
+            player = ActorFactory.CreateActor("Player");
+
+            janitor = new Actor("Janitor", @"Textures/Actors/janitor_normal.png");
+
+
+            AddChild(background);
+            RegisterActor(player);
+            RegisterActor(janitor);
+            RegisterActor(principal);
+        }
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+
+            janitor.DrawColor = new OpenTK.Graphics.Color4(1f, 1f, 1f, 0f);
+            principal.DrawColor = new OpenTK.Graphics.Color4(1f, 1f, 1f, 0f);
+        }
     }
 }
