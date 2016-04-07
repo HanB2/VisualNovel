da075f06f8493fd54c435fa640769995ebd06cc3
Trevor Fisher <minalear@gmail.com>
2016-04-07 17:13:21 -0500

    Begin work on improved UI elements and Animations

---
+++ DongLife/Animations/Animation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Minalear;
+
+namespace DongLife.Animations
+{
+    public abstract class Animation
+    {
+        public abstract void Update(GameTime gameTime);
+    }
+}
---
+++ DongLife/Scenes/TestScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Minalear;
+using Minalear.UI;
+using Minalear.UI.Controls;
+using DongLife.Controls;
+
+namespace DongLife.Scenes
+{
+    public class TestScene : Scene
+    {
+        public TestScene() : base("TestScene")
+        {
+
+        }
+    }
+}
